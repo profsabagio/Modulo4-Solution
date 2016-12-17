a503f404acfb79a169b8d17a61112b508d49ce11
Giovanni Saba <profsabagio@gmail.com>
2016-12-17 22:49:07 +0100

    ese4

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ for (var i = 0; i < names.length; i++) {
   
   var firstLetter = names[i].charAt(0).toLowerCase();
   
-  if (firstLetter === 'j') {
+  if (firstLetter=="j" || firstLetter=="J") {
     byeSpeaker.speak(names[i]);
   } 
   else {
